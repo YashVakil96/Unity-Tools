6b53718e742458c6d7120bb3d24f61b42fe63e03
Yash Vakil <yashvakil96@gmail.com>
2024-08-19 16:27:13 +0530

    Initial Commit
    Utility
    - Singleton
    Feature
    - Inspector Lock
    - Tool to Create Folder for fresh Unity Project

---
+++ Editor/ToolsMenu.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static UnityEngine.Application;
+using static UnityEditor.AssetDatabase;
+
+namespace Yash
+{
+    public static class ToolsMenu
+    {
+        [MenuItem("Tools/Setup/Create Default Folders")]
+        public static void CreateDefaultFolders()
+        {
+            CreateDirectories("_Project", "Scripts", "Art", "Scenes");
+            Refresh();
+        }
+
+        public static void CreateDirectories(string root, params string[] dir)
+        {
+            var fullpath = Combine(dataPath, root);
+            
+            foreach (var newDir in dir)
+                CreateDirectory(Combine(fullpath, newDir));
+        }
+    }
+}
